6d439d0f93236c3f70a2f1354ef0e267469b2810
Connor Gallopo <connor.gallopo@me.com>
2017-11-09 18:14:18 +0000

    automatically backed up by learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -4,6 +4,6 @@ def oxford_comma(array)
   elsif array.length == 2
     array.join(" and ")
   elsif array.length == 3
-    array.map!{|3| "and"}.join(" ,")
+    array.map!{ |3| "and"}.join(" ,")
   end
 end
